4572fd83e9d90f6898212ad5c871995b81e5819f
Clement Morel <cmorel@3G3.42angouleme.fr>
2026-02-04 11:20:28 +0100

    main.c for the bonus

--- bonus0/main.c
+++ bonus0/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+<<<<<<< HEAD
 #include <stdlib.h>
 
 void p(char *dest, char *s)
@@ -11,10 +12,22 @@ void p(char *dest, char *s)
     read(0, buffer, 4096);
     *strchr(buffer, '\n') = '\0';
     strncpy(dest, buffer, 20);
+=======
+
+void p(char *dest, char *c)
+{
+	char s[4096];
+	puts(c);
+	read(0, s, 4096);
+	char *sub = strchr(s, '\n');
+	*sub = '\0';
+	strncpy(dest, s, 20);
+>>>>>>> d38cc17 (bonus0 done)
 }
 
 void pp(char *dest)
 {
+<<<<<<< HEAD
 	char s1[20];
 	char s2[20];
     
@@ -37,4 +50,23 @@ int main(void)
     pp(dest);
     puts(dest);
     return 0;
+=======
+	char s[28];
+	char s2[28];
+	p(s, "-");
+	p(s2, "-");
+
+	strcpy(dest, s);
+	int len = strlen(dest);
+	strcat(dest, s2);
+	dest[len] = ' ';
+}
+
+int main(void)
+{
+	char s[22];
+
+	pp(s);
+	puts(s);
+>>>>>>> d38cc17 (bonus0 done)
 }
