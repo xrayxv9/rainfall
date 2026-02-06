6dd7e422c0f2f5abf207d80c234640123f985770
nda-cunh <hydraldev@gmail.com>
2026-02-06 23:48:58 +0100

    Add bonus2/main.c

--- bonus2/main.c
+++ bonus2/main.c
@@ -1,39 +1,58 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define FINNISH 1
-#define NETHERLAND 2
-#define DEFAULT 3
-
-int m = DEFAULT;
-
-void greetuser()
+typedef enum Langs
 {
-	char s2[16] = {0};
-	char s[88];
+	FINNISH = 1,
+	NETHERLAND = 2,
+	DEFAULT = 3
+}		Langs;
 
-	switch (m){
+Langs m = DEFAULT;
+
+void	greetuser(char *username)
+{
+	char	s1[30];
+
+	switch (m)
+	{
 		case DEFAULT:
-			memcpy(s2, "Hello ", 19);
+			strcpy(s1, "Hello ");
+			break ;
+		case FINNISH:
+			strcpy(s1, "Hyvää päivää ");
+			break ;
+		case NETHERLAND:
+			strcpy(s1, "Goedemiddag! ");
+			break ;
 	}
-
+	strcat(s1, username);
+	puts(s1);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-	char s[160] = {0};
+	char main_buffer[80];
+	char	*lang;
+
 	if (ac != 3)
-		return 1;
-	strncpy(s + 80, av[1], 40);
-	strncpy(s + 40, av[1], 40);
-	char *lang = getenv("LANG");
+		return (1);
+	memset(main_buffer, 0, 76);
+	strncpy(main_buffer, av[1], 40);
+	strncpy(main_buffer + 40, av[2], 32);
+	lang = getenv("LANG");
+		if (lang != NULL) {
 
-	if (!memcmp(lang, "fi", 2))
-		m = FINNISH;
-	else if (!memcmp(lang, "nl", 2))
-		m = NETHERLAND;
-	else
-		m = DEFAULT;	
-
-
+		if (!memcmp(lang, "fi", 2))
+			m = FINNISH;
+		else if (!memcmp(lang, "nl", 2))
+			m = NETHERLAND;
+		else
+			m = DEFAULT;
+	}
+	char temp_copy[76];
+    memcpy(temp_copy, main_buffer, 76);
+	greetuser(temp_copy);
+	return (0);
 }
